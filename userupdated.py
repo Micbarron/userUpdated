90b61640a0bdc94a23498f6f44d3b39ac34fe2a8
Michael Barron <michael.barron08@gmail.com>
2021-07-08 13:43:45 -0500

    final

--- userupdated.py
+++ userupdated.py
@@ -3,16 +3,20 @@ class User:
         self.name = name
         self.email = email
         self.account_balance = 0
+    
 
     def make_deposit(self, amount):
         self.account_balance += amount
+        return self
 
     def make_withdrawal(self, amount):
-        self.account_balance -= amount\
+        self.account_balance -= amount
+        return self
     
 
     def display_user_balance(self):
         print(self.account_balance)
+
 
 
 
